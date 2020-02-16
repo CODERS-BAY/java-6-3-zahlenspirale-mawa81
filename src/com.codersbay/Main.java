c5c7e65f4d15b2cd172bf97075ec8be3d97169e8
Mario Wagner <ma_wa@gmx.at>
2020-02-16 21:40:19 +0100

    Added the print loop

--- src/com.codersbay/Main.java
+++ src/com.codersbay/Main.java
@@ -3,13 +3,13 @@ package com.codersbay;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 3; // max N
+        int n = 5; // max N
         int xs_start = 1;
         int ys_start = 1;
         int xs_end = n;
         int ys_end = n;
 
-        int[][] field = new int[n * n][2];
+        //  int[][] field = new int[n * n][2];
         int[][] field_new = new int[n * n][2];
         int index = 0;
 
@@ -17,28 +17,27 @@ public class Main {
             for (int xs = xs_start; xs <= xs_end; xs++) {
                 field_new[index][0] = xs;
                 field_new[index][1] = xs_start;
-
-                System.out.println("i+: " + index);
+/*                System.out.println("i+: " + index);
                 System.out.println(field_new[index][0]);
-                System.out.println(field_new[index][1]);
+                System.out.println(field_new[index][1]);*/
                 index++;
             }
             ys_start++;
             for (int ys = ys_start; ys <= ys_end; ys++) {
                 field_new[index][0] = xs_end;
                 field_new[index][1] = ys;
-                System.out.println("i++: " + index);
+/*                System.out.println("i++: " + index);
                 System.out.println(field_new[index][0]);
-                System.out.println(field_new[index][1]);
+                System.out.println(field_new[index][1]);*/
                 index++;
             }
             xs_end--;
             for (int xs = xs_end; xs >= xs_start; xs--) {
                 field_new[index][0] = xs;
                 field_new[index][1] = ys_end;
-                System.out.println("i-: " + index);
+/*                System.out.println("i-: " + index);
                 System.out.println(field_new[index][0]);
-                System.out.println(field_new[index][1]);
+                System.out.println(field_new[index][1]);*/
                 index++;
                 // System.out.print(xs+" x "+xs_start);
             }
@@ -46,26 +45,28 @@ public class Main {
             for (int ys = ys_end; ys >= ys_start; ys--) {
                 field_new[index][0] = xs_start;
                 field_new[index][1] = ys;
-                System.out.println("i--: " + index);
+/*                System.out.println("i--: " + index);
                 System.out.println(field_new[index][0]);
-                System.out.println(field_new[index][1]);
+                System.out.println(field_new[index][1]);*/
                 //  System.out.print(ys_start+" y "+ys);
                 index++;
             }
             xs_start++;
         }
-        for (int e = 0; e < field_new.length; e++) {
+       /* for (int e = 0; e < field_new.length; e++) {
             System.out.println("i " + (e + 1));
             System.out.println(field_new[e][0]);
             System.out.println(field_new[e][1]);
-        }
-        index = 0;
+        } */
+        // goes through the x and y coordinates, loop through the field array if field x y similar to the loop x y
         for (int y = 1; y <= n; y++) {
             for (int x = 1; x <= n; x++) {
-                System.out.print(x + "" + y + " ");
-                field_new[index][1] = x;
-                index++;
-                System.out.print("index " + index + " ");
+                for (int i = 0; i < field_new.length; i++) {
+                    if (field_new[i][0] == x && field_new[i][1] == y) {
+                        int output = i + 1;
+                        System.out.printf("%2d ", output);    //print the index
+                    }
+                }
             }
             System.out.println();
         }
